6af09d5207960c2c802d29c8174718f001f02297
Hamza Haddani <hhamza@student.1337.ma>
2024-12-02 18:17:14 +0100

    feat: Add basic directory listing functionality
    - The program now accepts a path from commandline arguments
    - If no argument is specified, the default path is the current directory "."
    - The program lists the non-hidden contents separated by newlines

--- ft_ls.c
+++ ft_ls.c
@@ -1,7 +1,38 @@
 #include "libft/libft.h"
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	ft_putstr("Hello, World!\n");
+	char *path = ".";
+
+	if (argc == 2) {
+		path = argv[1];
+	}
+
+	DIR *dirp = opendir(path);
+
+	if (dirp == NULL) {
+		perror("opendir");
+		exit(EXIT_FAILURE);
+	}
+
+	struct dirent *entry = readdir(dirp);
+
+	while (entry != NULL) {
+		const char *path = entry->d_name;
+		const t_bool is_hidden = path[0] == '.';
+
+		if (!is_hidden) {
+			ft_printf("%s\n", path);
+		}
+
+		entry = readdir(dirp);
+	}
+
+	closedir(dirp);
 	return 0;
 }
