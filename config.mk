97847e2b2a9fab6bbb5698306fda57042be26912
Hamza Haddani <hhamza@student.1337.ma>
2024-12-03 21:10:33 +0100

    build(Makefile): Add memory leaks rule
    - Use valgrind to check for memory leaks and add the necessary flags to config.mk
    - Add Makefile rule to check for memory leaks
    - Arguments can be passed via ARGS environment variable. Example: ARGS="/home" make leaks

--- config.mk
+++ config.mk
@@ -7,9 +7,13 @@ LIBFT := libft/libft.a
 
 CC     := gcc
 FORMAT := clang-format
+LEAKS  := valgrind
 
 CFLAGS      := -Wall -Wextra -Werror
 FORMATFLAGS := -i
+LEAKSFLAGS  := --leak-check=full \
+			   --track-origins=yes \
+			   --show-leak-kinds=all
 
 ifdef DEBUG
 	CFLAGS += -Og -g
